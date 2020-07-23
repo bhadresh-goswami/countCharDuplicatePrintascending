2b1bf45d8522e65343751c11198d6c4ccc13eabb
bhadresh gosai <45452903+bhadresh-goswami@users.noreply.github.com>
2020-07-23 16:33:26 -0400

    upgraded

--- app.js
+++ app.js
@@ -1,55 +1,42 @@
 function removeduplicates(inputData) {
 
-    //split the string char in array
     var charArr = inputData.split('');
     var strArr = [];
     var output = [];
 
+    strArr = charArr.map((ele) => {
+        return ele.toLowerCase();
+    });
 
-    //convert each char in small case
-    for (let index = 0; index < charArr.length; index++) {
-        const element = charArr[index];
-        charArr[index] = element.toLowerCase();
-        if (charArr[index] != ' ') {
-            strArr.push(charArr[index]);
-        }
-    }
+    strArr = strArr.filter((ele) => {
+        var code = ele.charCodeAt(0);
+        return (code >= 61 && code <= 122);
+    });
 
-    //loop will find the array char count
+
     for (let i = 0; i < strArr.length; i++) {
-        //store char and will check with other char in array
         const element = strArr[i];
         var c = 0;
 
-        //inner loop that execute and match the one char to all other in loop
-        //from start next char in array j = i
         for (let j = i; j < strArr.length; j++) {
             const inelement = strArr[j];
             if (element == inelement) {
-                //if element found than need to remove from list so not
-                //come again in counting so replaced it to space
                 strArr[j] = ' ';
-                //count will incresed +1 if element found
                 c++;
             }
         }
-        //if element is the space then no need to consider in output
-        //if count 1 than also no need to add it in the output
-        if (element == " " || c == 1) {
-            continue;
-        }
-
-        //create an object to store values
-        
         var obj = {
             'ch': element,
             'c': c
         };
         output.push(obj);
-        //output+= `${element} ${c}\n`;
     }
-    
-    //need to sort element in char wise asc order
+
+    output = output.filter((ele) => {
+        var code = ele['ch'].charCodeAt(0);
+        return (code >= 61 && code <= 122) && ele['c']>1;
+    });
+
     output.sort(
         (a, b) => {
             if (a['ch'] > b['ch']) {
@@ -62,7 +49,6 @@ function removeduplicates(inputData) {
 
 
     var str = "";
-    //convert that object in string and return
     for (let i = 0; i < output.length; i++) {
         const element = output[i];
         str += `${element.ch} ${element.c}\n`;
